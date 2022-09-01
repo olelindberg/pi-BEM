36621bdffd9bcefd151d78d916b1196e054c91d2
Ole Lindberg <lindberg.ole@gmail.com>
2022-09-01 22:36:55 +0200

    HBIE Guiggiani 1992 Examples - as good as it gets ...

--- docs/python/HBIE_Guiggiani1992Examples/HBIE_Guiggiani1992Examples.py
+++ docs/python/HBIE_Guiggiani1992Examples/HBIE_Guiggiani1992Examples.py
@@ -30,19 +30,45 @@ def PolynomialBasis2d(xi1,xi2):
 
 class MeshGen41():
     def __init__(self):
-        return
-
-    def position(self,xi1,xi2):
         ypoints  = np.transpose(np.array([[-1,-1],[1.5,-1],[-1,1],[0.5,1]]))
         xipoints = np.transpose(np.array([[-1,-1],[1,-1],[-1,1],[1,1]]))
         A = np.zeros((4,4))
         for i in range(4):
             A[i,:] = PolynomialBasis2d(xipoints[0,i],xipoints[1,i])
         coeffs= np.linalg.solve(A,np.transpose(ypoints))
-        eval = np.transpose(coeffs)@PolynomialBasis2d(xi1,xi2)
+        self._a = coeffs[:,0]
+        self._b = coeffs[:,1]
+        return
 
-        pos = np.array([eval[0],0,eval[1]])
+    def position(self,xi1,xi2):
+        x = np.transpose(self._a)@PolynomialBasis2d(xi1,xi2)
+        y = np.transpose(self._b)@PolynomialBasis2d(xi1,xi2)
+        pos = np.array([x,y,0])
         return pos
+
+    def derivatives(self,xi1,xi2):
+        a = self._a
+        b = self._b
+        x_xi1    =  np.array([a[1] + a[3]*xi2, b[1] + b[3]*xi2, 0])
+        x_xi2    =  np.array([a[2] + a[3]*xi1, b[2] + b[3]*xi1, 0])
+        x_xi1xi1 =  np.array([0, 0, 0])
+        x_xi1xi2 =  np.array([a[3], b[3], 0])
+        x_xi2xi2 =  np.array([0, 0, 0])
+        return x_xi1,x_xi2,x_xi1xi1,x_xi1xi2,x_xi2xi2
+
+    def jacobian(self,xi1,xi2):
+        a = self._a
+        b = self._b
+        jac     =  np.array([0, 0, (a[1] + a[3]*xi2)*(b[2] + b[3]*xi1) - (a[2] + a[3]*xi1)*(b[1] + b[3]*xi2)])
+        return jac
+
+    def jacobian_derivatives(self,xi1,xi2):
+        a = self._a
+        b = self._b
+        jac_xi1 =  np.array([0, 0, -a[3]*(b[1] + b[3]*xi2) + b[3]*(a[1] + a[3]*xi2)])
+        jac_xi2 =  np.array([0, 0, a[3]*(b[2] + b[3]*xi1) + b[3]*(-a[2] - a[3]*xi1)])    
+        return jac_xi1,jac_xi2
+
 
 class MeshGen42():
     def __init__(self):
@@ -75,9 +101,8 @@ class MeshGen42():
         return jac_xi1,jac_xi2
 
 N = 3
-n = 10
-dim = 1
-example = ["4.2","a"]
+n = 16
+example = ["4.2","c"]
 if example[0]=="4.1":
     mesh = MeshGen41()
     if example[1] =="a":
@@ -107,60 +132,12 @@ print("y ", y)
 xi1d      = np.linspace(-1,1,N)
 xi1,xi2 = np.meshgrid(xi1d,xi1d)
 xi = np.array([xi1.flatten(),xi2.flatten()])
-x = np.zeros((3,9))
 
-# x_xi1     = np.zeros((9,3)) 
-# x_xi2     = np.zeros((9,3))
-# x_xi1xi1  = np.zeros((9,3))
-# x_xi2xi1  = np.zeros((9,3))
-# x_xi2xi2  = np.zeros((9,3))
-
-# jac       = np.zeros((9,3)) 
-# jac_xi1   = np.zeros((9,3)) 
-# jac_xi2   = np.zeros((9,3))
-
-for i in range(9):
-    x[:,i] = mesh.position(xi1.flatten()[i],xi2.flatten()[i])
-    # derivative = mesh.derivatives(xi1.flatten()[i],xi2.flatten()[i])
-    # x_xi1[i,:]    = derivative[0]
-    # x_xi2[i,:]    = derivative[1]
-    # x_xi1xi1[i,:] = derivative[2]
-    # x_xi2xi1[i,:] = derivative[3]
-    # x_xi2xi2[i,:] = derivative[4]
-
-    # jac[i,:]    = mesh.jacobian(xi1.flatten()[i],xi2.flatten()[i])
-    # jacobian_derivatives = mesh.jacobian_derivatives(xi1.flatten()[i],xi2.flatten()[i])
-    # jac_xi1[i,:]    = jacobian_derivatives[0]
-    # jac_xi2[i,:]    = jacobian_derivatives[1]
-
-#edgeloop = [0,1,2,5,8,7,6,3]
 edgeloop = [0,2,8,6]
 
 #------------------------------------------------------------------------------
-# Derivative matrices:
+# Derivatives at eta:
 #------------------------------------------------------------------------------
-
-# # First derivatives:
-D1,D2 = Lagrange2DDerivativeMatrices(xi1d,1)
-
-# # Second derivatives:
-D11 = D1@D1
-D12 = D1@D2
-D22 = D2@D2
-
-x_xi1    = D1@np.transpose(x) 
-x_xi2    = D2@np.transpose(x) 
-x_xi1xi1 = D11@np.transpose(x) 
-x_xi2xi1 = D12@np.transpose(x) 
-x_xi2xi2 = D22@np.transpose(x)
-jac = 0*x_xi1
-for i in range(x_xi1.shape[0]): 
-    jac[i,:] = np.cross(x_xi1[i,:],x_xi2[i,:])
-jac_xi1  = D1@jac
-jac_xi2  = D2@jac
-
-interpToEta = Lagrange2DInterpMatrix(xi1d,xi1d,eta[0],eta[1]).flatten()
-
 derivative = mesh.derivatives(eta[0],eta[1])
 x_xi1_eta    = derivative[0]
 x_xi2_eta    = derivative[1]
@@ -168,39 +145,20 @@ x_xi1xi1_eta = derivative[2]
 x_xi2xi1_eta = derivative[3]
 x_xi2xi2_eta = derivative[4]
 
-
-# x_xi1_eta    = interpToEta@x_xi1   
-# x_xi2_eta    = interpToEta@x_xi2   
-# x_xi1xi1_eta = interpToEta@x_xi1xi1
-# x_xi2xi1_eta = interpToEta@x_xi2xi1
-# x_xi2xi2_eta = interpToEta@x_xi2xi2
-# jac_eta      = interpToEta@jac
-# jac_xi1_eta  = interpToEta@jac_xi1
-# jac_xi2_eta  = interpToEta@jac_xi2
-
 jac_eta     = mesh.jacobian(eta[0],eta[1])
 derivative  = mesh.jacobian_derivatives(eta[0],eta[1])
 jac_xi1_eta = derivative[0]
 jac_xi2_eta = derivative[1] 
 
-Na_eta      = interpToEta
-Na_xi1_eta  = interpToEta@D1
-Na_xi2_eta  = interpToEta@D2
-
 gaussx,gaussw = np.polynomial.legendre.leggauss(n)
 
-xx = []
-yy = []
 I0 = 0
 Im1 = 0
 Im2 = 0
 for i in range(len(edgeloop)):
 
-    i1 = edgeloop[i]
-    i2 = edgeloop[(i+1)%len(edgeloop)]
-
-    v1 = xi[:,i1]
-    v2 = xi[:,i2]
+    v1 = xi[:,edgeloop[i]]
+    v2 = xi[:,edgeloop[(i+1)%len(edgeloop)]]
 
     r1 = v1 - eta
     r2 = v2 - eta
@@ -228,19 +186,13 @@ for i in range(len(edgeloop)):
         Ji0 = jac_eta
         Ji1 = jac_xi1_eta*np.cos(theta) + jac_xi2_eta*np.sin(theta)        
 
-        Na0 = Na_eta
-        Na1 = Na_xi1_eta*np.cos(theta) + Na_xi2_eta*np.sin(theta)
-
         beta = 1/A
         gamma = -np.inner(Ai,Bi)/A**4
 
         gi1 = Ai/A**2*(Bi@Ji0 + Ai@Ji1)
 
-        bi0 = -Ji0
-        bi1 = 3*gi1 - Ji1
-
-        ai0 = np.outer(bi0,Na0)
-        ai1 = np.outer(bi1,Na0) + np.outer(bi0,Na1)
+        ai0 = -Ji0
+        ai1 = 3*gi1 - Ji1
 
         Sm3 = 1/A**3
         Sm2 = - 3*np.inner(Ai,Bi)/A**5
@@ -248,9 +200,6 @@ for i in range(len(edgeloop)):
         Fm2 = -1/(4*np.pi)*Sm3*ai0
         Fm1 = -1/(4*np.pi)*(Sm2*ai0+Sm3*ai1)
         
-        #Fm2 = 1/(4*np.pi)*Ji0/A**3
-        #Fm1 = 1/(4*np.pi)*(-3*C*Ji0/A**5 - 3*Ai/A**5*(np.inner(Ji0,Bi) + np.inner(Ji1,Ai)) + Ji1 / A**3)
-
         dtheta = (theta2-theta1)/2*theta_gw
         Im2 += - Fm2*(gamma/beta**2+1/rho_hat)*dtheta
         Im1 += Fm1*np.log(rho_hat/beta)*dtheta
@@ -267,44 +216,30 @@ for i in range(len(edgeloop)):
             rvec = xxx-y
             r = np.linalg.norm(rvec)
             ri = rvec/r
-            Na = Lagrange2DInterpMatrix(xi1d,xi1d,xi1,xi2).flatten()
             Ji = mesh.jacobian(xi1,xi2)
-#            Ji = Na@jac
             J  = np.linalg.norm(Ji)
             ni = Ji/J
-            F = - 1/(4*np.pi*r**3)*np.outer((3*ri*np.inner(ri,ni) - ni),Na)*J*rho
+            F = - 1/(4*np.pi*r**3)*(3*ri*np.inner(ri,ni) - ni)*J*rho
 
             drho = rho_hat/2*rho_gw
             I0 += (F - (Fm2/rho**2 + Fm1/rho))*drho*dtheta
 
-            xx.append(xi1)
-            yy.append(xi2)
-
-I = np.sum(I0+Im1+Im2,axis=1)
+I = I0+Im1+Im2
 
 if example[0]=="4.1":
-    print("I0      ", I0[dim]*4*np.pi)
-    print("Im1     ", Im1[dim]*4*np.pi)
-    print("Im2     ", Im2[dim]*4*np.pi)
-    print("I       ", I[dim]*4*np.pi)
+    print("I       ", I*4*np.pi)
     print("Iexact  ", Iexact)
-    print("err abs ", np.abs(Iexact- I[dim]*4*np.pi))
-    print("err rel ", np.abs(Iexact- I[dim]*4*np.pi)/np.abs(Iexact))
+    print("err abs ", np.abs(Iexact- I[2]*4*np.pi))
+    print("err rel ", np.abs(Iexact- I[2]*4*np.pi)/np.abs(Iexact))
 
 if example[0]=="4.2":
-    print("I0      ", I0[2])
-    print("Im1     ", Im1[2])
-    print("Im2     ", Im2[2])
     print("I       ", I)
     print("Iex     ", Iexact)
     print("err abs ", np.abs(Iexact- I[2]))
     print("err rel ", np.abs(Iexact- I[2])/np.abs(Iexact))
 
 ax = plt.axes(projection='3d')
-for i in range(len(xx)):
-    yyy = mesh.position(xx[i],yy[i])
-    ax.plot3D(np.array([yyy[0]]),np.array([yyy[1]]),np.array([yyy[2]]), 'go')
-ax.plot3D(x[0,:], x[1,:],x[2,:], 'bo')
+#ax.plot3D(x[0,:], x[1,:],x[2,:], 'bo')
 ax.plot3D(np.array([y[0]]),np.array([y[1]]),np.array([y[2]]), 'ro')
 ax.set_xlabel("x")
 ax.set_ylabel("y")
--- docs/python/HBIE_Guiggiani1992Examples/HBIE_Guiggiani1992ExamplesSympy.py
+++ docs/python/HBIE_Guiggiani1992Examples/HBIE_Guiggiani1992ExamplesSympy.py
@@ -1,55 +1,38 @@
 from sympy import *
 
-xi1,xi2 = symbols("xi1 xi2")
+def print_derivatives(x,y,z):
+
+    x_xi1    = [x.diff(xi1),y.diff(xi1),z.diff(xi1)]
+    x_xi2    = [x.diff(xi2),y.diff(xi2),z.diff(xi2)]
+    x_xi1xi1 = [x.diff(xi1,2),y.diff(xi1,2),z.diff(xi1,2)]
+    x_xi1xi2 = [x.diff(xi1).diff(xi2),y.diff(xi1).diff(xi2),z.diff(xi1).diff(xi2)]
+    x_xi2xi2 = [x.diff(xi2,2),y.diff(xi2,2),z.diff(xi2,2)]
+
+    jac = [x_xi1[1]*x_xi2[2] - x_xi1[2]*x_xi2[1],       
+           x_xi1[2]*x_xi2[0] - x_xi1[0]*x_xi2[2],
+           x_xi1[0]*x_xi2[1] - x_xi1[1]*x_xi2[0]]
+    jac_xi1 = [jac[0].diff(xi1),jac[1].diff(xi1),jac[2].diff(xi1)]
+    jac_xi2 = [jac[0].diff(xi2),jac[1].diff(xi2),jac[2].diff(xi2)]
+
+    print("x_xi1    = ",x_xi1   ) 
+    print("x_xi2    = ",x_xi2   ) 
+    print("x_xi1xi1 = ",x_xi1xi1) 
+    print("x_xi1xi2 = ",x_xi1xi2) 
+    print("x_xi2xi2 = ",x_xi2xi2) 
+    print("jac     = ",jac) 
+    print("jac_xi1 = ",jac_xi1) 
+    print("jac_xi2 = ",jac_xi2)  
+
+xi1,xi2,a0,a1,a2,a3,b0,b1,b2,b3 = symbols("xi1 xi2 a0 a1 a2 a3 b0 b1 b2 b3")
 
 angle = pi/2*(xi2+1)/2
 x = cos(angle) 
 y = xi1 + 1
 z = sin(angle) 
 
-x_xi1    = x.diff(xi1)
-y_xi1    = y.diff(xi1)
-z_xi1    = z.diff(xi1)
-   
-x_xi2    = x.diff(xi2)
-y_xi2    = y.diff(xi2)
-z_xi2    = z.diff(xi2)
+x = a0+a1*xi1+a2*xi2+a3*xi1*xi2
+y = b0+b1*xi1+b2*xi2+b3*xi1*xi2
+z = 0*xi1
 
-x_xi1xi1 = x.diff(xi1,2)
-y_xi1xi1 = y.diff(xi1,2)
-z_xi1xi1 = z.diff(xi1,2)
+print_derivatives(x,y,z)
 
-x_xi2xi2 = x.diff(xi2,2)
-y_xi2xi2 = y.diff(xi2,2)
-z_xi2xi2 = z.diff(xi2,2)
-
-x_xi1xi2 = x.diff(xi1).diff(xi2)
-y_xi1xi2 = y.diff(xi1).diff(xi2)
-z_xi1xi2 = z.diff(xi1).diff(xi2)
-
-
-jac = [y_xi1*z_xi2 - z_xi1*y_xi2,       z_xi1*x_xi2 - x_xi1*z_xi2,       x_xi1*y_xi2 - y_xi1*x_xi2]
-jac_xi1 = [jac[0].diff(xi1),jac[1].diff(xi1),jac[2].diff(xi1)]
-jac_xi2 = [jac[0].diff(xi2),jac[1].diff(xi2),jac[2].diff(xi2)]
-
-
-
-print("x_xi1    = ",x_xi1   ) 
-print("y_xi1    = ",y_xi1   ) 
-print("z_xi1    = ",z_xi1   ) 
-print("x_xi2    = ",x_xi2   ) 
-print("y_xi2    = ",y_xi2   ) 
-print("z_xi2    = ",z_xi2   ) 
-print("x_xi1xi1 = ",x_xi1xi1) 
-print("y_xi1xi1 = ",y_xi1xi1) 
-print("z_xi1xi1 = ",z_xi1xi1) 
-print("x_xi2xi2 = ",x_xi2xi2) 
-print("y_xi2xi2 = ",y_xi2xi2) 
-print("z_xi2xi2 = ",z_xi2xi2) 
-print("x_xi1xi2 = ",x_xi1xi2) 
-print("y_xi1xi2 = ",y_xi1xi2) 
-print("z_xi1xi2 = ",z_xi1xi2) 
-
-print("jac     = ",jac) 
-print("jac_xi1 = ",jac_xi1) 
-print("jac_xi2 = ",jac_xi2) 
