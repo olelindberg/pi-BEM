777a1f3f1f52400009744ad80dc575042f1ec245
Ole Lindberg <lindberg.ole@gmail.com>
2022-09-29 08:44:41 +0200

    Integrates over the sphere instead of a ball in the C example. Its correct now

--- docs/python/HBIE_Mantic1996/HBIE_Mantic1996_C_Matrix.py
+++ docs/python/HBIE_Mantic1996/HBIE_Mantic1996_C_Matrix.py
@@ -1,21 +1,32 @@
 from sympy import *
-init_printing(use_unicode=True)
 r, phi, theta = symbols("r phi theta")
 
+phi_min = 0
+phi_max = 3/2*pi
+
+theta_min = 0
+theta_max = pi
+
+# Spherical coordinates:
 x = r*cos(phi)*sin(theta)
 y = r*sin(phi)*sin(theta)
 z = r*cos(theta)
-       
+
+# Jacobian of the coordinate transform:       
 jac = Matrix([  [diff(x,r),diff(x,theta),diff(x,phi)],
                 [diff(y,r),diff(y,theta),diff(y,phi)],
                 [diff(z,r),diff(z,theta),diff(z,phi)]]).det()
 
+# Outer product:
 xx = Matrix([[x*x,x*y,x*z],
-            [y*x,y*y,y*z],
-            [z*x,z*y,z*z]])
+             [y*x,y*y,y*z],
+             [z*x,z*y,z*z]])
 
-C11 = 3/(4*pi)*integrate(integrate(integrate(xx*jac,(phi,0,3/2*pi)),(theta,0,pi)),(r,0,1))
+# Integrate over unit sphere:
+C = 3/(4*pi)*integrate(integrate(xx*jac,(phi,phi_min,phi_max)),(theta,theta_min,theta_max))
 
+# Insert unit radius r=1:
+C = C.subs(r,1)
 
-
-print(C11)
+# Show result:
+print(C)
