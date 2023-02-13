c84bdb4a20ad05fcf17c0e33771a441660eac307
Ole Lindberg <lindberg.ole@gmail.com>
2023-02-13 07:46:49 +0100

    python scripts for animation

---
+++ docs/data/KCS_Limfjord/post/readvtu.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import vtkmodules.all as vtk
+
+def main():
+
+    colors = vtk.vtkNamedColors()
+
+    # Read the source file.
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName('/home/ole/Projects/pi-BEM/docs/data/KCS_Limfjord/output/result_scalar_results_0.vtu')
+    reader.Update()  # Needed because of GetScalarRange
+    output = reader.GetOutput()
+    # scalar_range = output.GetScalarRange()
+
+    # Create the mapper that corresponds the objects of the vtk.vtk file
+    # into graphics elements
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(output)
+    # mapper.SetScalarRange(scalar_range)
+    mapper.ScalarVisibilityOff()
+
+    # Create the Actor
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().EdgeVisibilityOn()
+    actor.GetProperty().SetLineWidth(2.0)
+    actor.GetProperty().SetColor(colors.GetColor3d("MistyRose"))
+
+    backface = vtk.vtkProperty()
+    backface.SetColor(colors.GetColor3d('Tomato'))
+    actor.SetBackfaceProperty(backface)
+
+    # Create the Renderer
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d('Wheat'))
+
+    # Create the RendererWindow
+    renderer_window = vtk.vtkRenderWindow()
+    renderer_window.SetSize(640, 480)
+    renderer_window.AddRenderer(renderer)
+    renderer_window.SetWindowName('ReadUnstructuredGrid')
+
+    # Create the RendererWindowInteractor and display the vtk_file
+    interactor = vtk.vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderer_window)
+    interactor.Initialize()
+    interactor.Start()
+
+
+if __name__ == '__main__':
+    main()
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName(file_name)
+    reader.Update()  # Needed because of GetScalarRange
+    output = reader.GetOutput()
+    # scalar_range = output.GetScalarRange()
+
+    # Create the mapper that corresponds the objects of the vtk.vtk file
+    # into graphics elements
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(output)
+    # mapper.SetScalarRange(scalar_range)
+    mapper.ScalarVisibilityOff()
+
+    # Create the Actor
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().EdgeVisibilityOn()
+    actor.GetProperty().SetLineWidth(2.0)
+    actor.GetProperty().SetColor(colors.GetColor3d("MistyRose"))
+
+    backface = vtk.vtkProperty()
+    backface.SetColor(colors.GetColor3d('Tomato'))
+    actor.SetBackfaceProperty(backface)
+
+    # Create the Renderer
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d('Wheat'))
+
+    # Create the RendererWindow
+    renderer_window = vtk.vtkRenderWindow()
+    renderer_window.SetSize(640, 480)
+    renderer_window.AddRenderer(renderer)
+    renderer_window.SetWindowName('ReadUnstructuredGrid')
+
+    # Create the RendererWindowInteractor and display the vtk_file
+    interactor = vtk.vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderer_window)
+    interactor.Initialize()
+    interactor.Start()
+
+
+if __name__ == '__main__':
+    main()
