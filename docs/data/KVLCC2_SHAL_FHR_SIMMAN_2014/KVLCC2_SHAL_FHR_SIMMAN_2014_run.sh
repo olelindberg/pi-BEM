524a535b460b8bf806ce4aafbc40520730e4a076
Ole Lindberg <lindberg.ole@gmail.com>
2023-04-25 14:37:22 +0200

    KVLCC2 SIMMAN 2014 examples runs 2

---
+++ docs/data/KVLCC2_SHAL_FHR_SIMMAN_2014/KVLCC2_SHAL_FHR_SIMMAN_2014_run.sh
@@ -0,0 +1,71 @@
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C2/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C3/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z05A01_C4/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C2/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C3/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A02_C4/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C2/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C3/mesh06
+
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh00
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh01
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh02
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh03
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh04
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh05
+mpiexec -np 4 ./../../../build/bem_fma_3d 4 --input-path T0Z02A03_C4/mesh06
